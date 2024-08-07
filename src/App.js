fabae78ea66e43442a220dd67674f6c1ff47762c
deepak29-git <dg887180@gmail.com>
2024-08-07 11:37:01 +0530

    added route

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
           }
         />
         <Route path={"/login"} element={<Login />} />
+        <Route path={"/"} element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </>
